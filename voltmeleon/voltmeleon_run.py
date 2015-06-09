a15324f11a227072442bb955967c412f313941ca
Guillaume Alain <gyomalin@gmail.com>
2015-06-09 16:46:45 -0400

    continue to reconcile old and new version of the interface

--- voltmeleon/voltmeleon_run.py
+++ voltmeleon/voltmeleon_run.py
@@ -1,54 +1,26 @@
 
 # TODO : import the needed modules
 
+import client_runner
+
 def usage():
-    print "THEANO_FLAGS=device=gpu0,floatX=float32 python client_standalone_svhn_ninjite.py --experiment_dir=/home/gyomalin/Documents/ddt14/ImageNet/ninjite/experiment_dir_1"
+    print "THEANO_FLAGS=device=gpu0,floatX=float32 python voltmeleon_run.py --experiment_dir=/home/gyomalin/NIPS/experiments/experiment_dir_1"
 
 
 def run(experiment_dir):
 
     assert os.path.exists(experiment_dir), "Cannot find experiment_dir : %s" % experiment_dir
 
-    experiment_desc_file = os.path.join(experiment_dir, "experiment_desc.json")
+    model_desc_file = os.path.join(experiment_dir, "model_desc.json")
+    train_desc_file = os.path.join(experiment_dir, "train_desc.json")
+
     import json
-    experiment_desc = json.load(open(experiment_desc_file, "r"))
+    model_desc = json.load(open(model_desc_file, "r"))
+    train_desc = json.load(open(train_desc_file, "r"))
+    # hardcoded path for blocks saving a zip file
+    saving_path = os.path.join(experiment_dir, "log.zip")
 
-    batch_size = experiment_desc['batch_size']
-    #learning_rate = experiment_desc['learning_rate']
-    #momentum = experiment_desc['momentum']
-    step_flavor = experiment_desc['step_flavor']    
-    integral_drop_rate = experiment_desc['integral_drop_rate']
-    flecked_drop_rate = experiment_desc['flecked_drop_rate']
-    L_nbr_filters = experiment_desc['L_nbr_filters']
-    L_nbr_hidden_units = experiment_desc['L_nbr_hidden_units']
-    weight_decay_factor = experiment_desc['weight_decay_factor'] if experiment_desc.has_key('weight_decay_factor') else 0.0
-    dataset_hdf5_file = experiment_desc['dataset_hdf5_file']
-    saving_path = experiment_desc['saving_path']
-
-    server_desc = experiment_desc['server'] if experiment_desc.has_key('server') else None
-    sync_desc = experiment_desc['sync'] if experiment_desc.has_key('sync') else None
-
-
-    optional_args = {}
-    for key in ['nbr_epochs']:
-        if experiment_desc.has_key(key):
-            optional_args[key] = experiment_desc[key]
-
-    assert 1 <= batch_size
-    assert os.path.exists(dataset_hdf5_file)
-
-    print "L_nbr_filters : " + str(L_nbr_filters) 
-    print "L_nbr_hidden_units : " + str(L_nbr_hidden_units) 
-
-    run_training(   batch_size, step_flavor,
-                    integral_drop_rate, flecked_drop_rate,
-                    L_nbr_filters, L_nbr_hidden_units,
-                    weight_decay_factor=weight_decay_factor,
-                    dataset_hdf5_file=dataset_hdf5_file,
-                    saving_path=saving_path,
-                    server_desc=server_desc,
-                    sync_desc=sync_desc,
-                    **optional_args)
+    client_runner.run(model_desc, train_desc, experiment_dir, saving_path)
 
 
 def main(argv):

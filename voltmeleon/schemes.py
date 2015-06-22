f68355f02c19a9487556175eabed27b7d3e2fe07
Melanie Ducoffe <ducoffem@boltzmann.iro.umontreal.ca>
2015-06-22 16:05:13 -0400

    add a new iteration scheme

---
+++ voltmeleon/schemes.py
@@ -0,0 +1,17 @@
+from abc import ABCMeta, abstractmethod
+import numpy
+from picklable_itertools import iter_
+
+@add_metaclass(ABCMeta)
+class LimitedScheme(IterationScheme):
+    """Iterate over the sequence given an iterationScheme object
+        but in a limited number of excursions defined by times
+    """
+    def __init__(self, iteration_scheme, times, *args, **kwargs):
+        self.iteration_scheme = iteration_scheme
+        self.times = times
+        assert self.times >=1, "Error : you need a positiv number of mini batches"
+
+    def get_request_iterator(self):
+        it = self.iteration_scheme.get_request_iterator()
+        return iter_([it.next() for n in range(self.times)]) 

cdf30bb4fd3c134a870a8a977b5de7c5eb1871cf
dpln@serendib <gyomalin@gmail.com>
2015-07-18 00:24:50 -0400

    launch script

--- voltmeleon/config_examples/helios-svhn/experiment_01/helios_cluster_script.sh
+++ voltmeleon/config_examples/helios-svhn/experiment_01/helios_cluster_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l nodes=4:gpus=2
+#PBS -l nodes=2:gpus=8
 #PBS -l walltime=12:00:00
 #PBS -A jvb-000-ag
 #PBS -M guillaume.alain.umontreal@gmail.com
@@ -18,9 +18,12 @@ export EXPERIMENT_DIR=${HOME}/NIPS/experiments/01
 cd ${EXPERIMENT_DIR}/voltmeleon/voltmeleon
 
 # Make sure you use a different device for those two calls.
+# DEBUG : Use only one GPU. This is wasteful, but let's try this for now
+#         and tomorrow we'll figure out a way to hash this thing out.
+#         (Maybe use the gpu number as part of the internal index.)
 PYTHONPATH=${PYTHONPATH}:${HOME}/NIPS/distdrop THEANO_FLAGS=device=gpu0,floatX=float32 python voltmeleon_run.py --experiment_dir=config_examples/helios-svhn/experiment_01 --helios &
 
-PYTHONPATH=${PYTHONPATH}:${HOME}/NIPS/distdrop THEANO_FLAGS=device=gpu1,floatX=float32 python voltmeleon_run.py --experiment_dir=config_examples/helios-svhn/experiment_01 --helios &
+#PYTHONPATH=${PYTHONPATH}:${HOME}/NIPS/distdrop THEANO_FLAGS=device=gpu1,floatX=float32 python voltmeleon_run.py --experiment_dir=config_examples/helios-svhn/experiment_01 --helios &
 
 # taken from
 # http://stackoverflow.com/questions/356100/how-to-wait-in-bash-for-several-subprocesses-to-finish-and-return-exit-code-0

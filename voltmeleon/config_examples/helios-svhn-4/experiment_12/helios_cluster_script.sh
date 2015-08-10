60bf9eba3b59caedd5aab1e88c764b4e957f73a4
Guillaume Alain <dpln@lambda>
2015-08-10 17:17:39 -0400

    adding svhn-4 experiment 08-15

---
+++ voltmeleon/config_examples/helios-svhn-4/experiment_12/helios_cluster_script.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#PBS -l nodes=1:gpus=2
+#PBS -l walltime=12:00:00
+#PBS -A jvb-000-ag
+#PBS -m bea
+#PBS -t [0-1]%2
+
+"""#PBS -M guillaume.alain.umontreal@gmail.com"""
+
+# This whole script assumes that you've started the server
+# on `helios1` the entry node for the Helios cluster.
+# You should have picked a port that nobody else was using.
+
+
+# To run this manually at home (dpln@lambda) :
+#
+#/home/dpln/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/voltmeleon/voltmeleon/config_examples/helios-svhn/scratch/server_params_desc.json --port=7000
+#
+#cd /home/dpln/NIPS/distdrop/bin/
+#python auto_init_server_params.py --server=127.0.0.1 --port=7000 --W_range=0.1 --b_range=0.1 --want_zero_momentum
+#
+#cd ${HOME}/NIPS/voltmeleon/voltmeleon
+#THEANO_FLAGS=device=gpu0,floatX=float32 python voltmeleon_run.py --experiment_dir=config_examples/helios-svhn/scratch
+
+
+export EXPID=12
+
+
+export EXPERIMENT_DIR=${HOME}/NIPS/experiments-svhn-4/${EXPID}
+# Assuming that we've previously performed a
+# git clone https://github.com/gyom/voltmeleon.git ${EXPERIMENT_DIR}/voltmeleon
+# into that directory in preparation.
+cd ${EXPERIMENT_DIR}/voltmeleon/voltmeleon
+
+export RELATIVE_CONFIG_DIR=config_examples/helios-svhn-4/experiment_${EXPID}
+
+# Make sure you use a different device for those two calls.
+# DEBUG : Use only one GPU. This is wasteful, but let's try this for now
+#         and tomorrow we'll figure out a way to hash this thing out.
+#         (Maybe use the gpu number as part of the internal index.)
+export PYTHONPATH=${PYTHONPATH}:${HOME}/NIPS/distdrop
+export PYTHONPATH=${PYTHONPATH}:${HOME}/deep-learning-suite/fuel
+export PYTHONPATH=${PYTHONPATH}:${HOME}/deep-learning-suite/theano
+export PYTHONPATH=${PYTHONPATH}:${HOME}/deep-learning-suite/picklable_itertools
+export PYTHONPATH=${PYTHONPATH}:${HOME}/deep-learning-suite/blocks
+
+# transfer the stdout to stderr to it gets logged
+THEANO_FLAGS=device=gpu0,floatX=float32 stdbuf -i0 -o0 -e0 python voltmeleon_run.py --experiment_dir=${RELATIVE_CONFIG_DIR} --helios >> ${MOAB_JOBARRAYINDEX}_0_out &
+
+THEANO_FLAGS=device=gpu1,floatX=float32 stdbuf -i0 -o0 -e0 python voltmeleon_run.py --experiment_dir=${RELATIVE_CONFIG_DIR} --helios >> ${MOAB_JOBARRAYINDEX}_1_out &
+
+# taken from
+# http://stackoverflow.com/questions/356100/how-to-wait-in-bash-for-several-subprocesses-to-finish-and-return-exit-code-0
+# because we want to wait for the two jobs running
+for job in `jobs -p`
+do
+echo $job
+    wait $job
+done
+
+cat ${MOAB_JOBARRAYINDEX}_0_out
+cat ${MOAB_JOBARRAYINDEX}_1_out
+
+#rm ${MOAB_JOBARRAYINDEX}_0_out
+#rm ${MOAB_JOBARRAYINDEX}_1_out
--- voltmeleon/config_examples/helios-svhn-4/notes_08_15.sh
+++ voltmeleon/config_examples/helios-svhn-4/notes_08_15.sh
@@ -1,5 +1,15 @@
 
-for EXPID in 00 01 02 03 04 05 06 07 ;
+
+
+cd ${HOME}/NIPS/voltmeleon/voltmeleon
+THEANO_FLAGS=device=gpu0,floatX=float32 python voltmeleon_run.py --experiment_dir=config_examples/helios-svhn-4/experiment_08 --output_server_params_desc_path=config_examples/helios-svhn-4/experiment_08/server_params_desc.json
+
+
+
+
+
+
+for EXPID in 08 09 10 11 12 13 14 15 ;
 do
     mkdir ${HOME}/NIPS/experiments-svhn-4
     mkdir ${HOME}/NIPS/experiments-svhn-4/${EXPID}
@@ -8,7 +18,7 @@ do
 done
 
 
-for EXPID in 00 01 02 03 04 05 06 07 ;
+for EXPID in 08 09 10 11 12 13 14 15 ;
 do
     cd ${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon
     git pull
@@ -19,56 +29,56 @@ done
 
 
 
-export EXPID=00
-export EXPPORT=7400
+export EXPID=08
+export EXPPORT=7408
 /home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
 
-export EXPID=01
-export EXPPORT=7401
+export EXPID=09
+export EXPPORT=7409
 /home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
 
-export EXPID=02
-export EXPPORT=7402
+export EXPID=10
+export EXPPORT=7410
 /home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
 
-export EXPID=03
-export EXPPORT=7403
-/home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
-
-
-export EXPID=04
-export EXPPORT=7404
-/home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
-
-export EXPID=05
-export EXPPORT=7405
-/home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
-
-export EXPID=06
-export EXPPORT=7406
-/home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
-
-export EXPID=07
-export EXPPORT=7407
+export EXPID=11
+export EXPPORT=7411
 /home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
 
 
+export EXPID=12
+export EXPPORT=7412
+/home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
+
+export EXPID=13
+export EXPPORT=7413
+/home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
+
+export EXPID=14
+export EXPPORT=7414
+/home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
+
+export EXPID=15
+export EXPPORT=7415
+/home/alaingui/NIPS/distdrop/bin/server --model_params_desc=${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}/server_params_desc.json --port=${EXPPORT}
 
 
 
-python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7400 --W_range=0.1 --b_range=0.1 --want_zero_momentum
-python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7401 --W_range=0.1 --b_range=0.1 --want_zero_momentum
-python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7402 --W_range=0.1 --b_range=0.1 --want_zero_momentum
-python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7403 --W_range=0.1 --b_range=0.1 --want_zero_momentum
-python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7404 --W_range=0.1 --b_range=0.1 --want_zero_momentum
-python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7405 --W_range=0.1 --b_range=0.1 --want_zero_momentum
-python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7406 --W_range=0.1 --b_range=0.1 --want_zero_momentum
-python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7407 --W_range=0.1 --b_range=0.1 --want_zero_momentum
+
+
+python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7408 --W_range=0.1 --b_range=0.1 --want_zero_momentum
+python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7409 --W_range=0.1 --b_range=0.1 --want_zero_momentum
+python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7410 --W_range=0.1 --b_range=0.1 --want_zero_momentum
+python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7411 --W_range=0.1 --b_range=0.1 --want_zero_momentum
+python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7412 --W_range=0.1 --b_range=0.1 --want_zero_momentum
+python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7413 --W_range=0.1 --b_range=0.1 --want_zero_momentum
+python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7414 --W_range=0.1 --b_range=0.1 --want_zero_momentum
+python ${HOME}/NIPS/distdrop/bin/auto_init_server_params.py --server=127.0.0.1 --port=7415 --W_range=0.1 --b_range=0.1 --want_zero_momentum
 
 
 
 
-for EXPID in 00 01 02 03 04 05 06 07 ;
+for EXPID in 08 09 10 11 12 13 14 15 ;
 do
     cd ${HOME}/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID}
     msub helios_cluster_script.sh
@@ -82,7 +92,7 @@ done
 
 
 
-for EXPID in 00 01 02 03 04 05 06 07 ;
+for EXPID in 08 09 10 11 12 13 14 15 ;
 do
     rsync -av --delete helios:/home/alaingui/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/config_examples/helios-svhn-4/experiment_${EXPID} /home/dpln/NIPS/experiments-svhn-4
     rsync -av helios:/home/alaingui/NIPS/experiments-svhn-4/${EXPID}/voltmeleon/voltmeleon/'*_*_out' /home/dpln/NIPS/experiments-svhn-4/experiment_${EXPID}
@@ -90,7 +100,7 @@ done
 
 
 
-for EXPID in 00 01 02 03 04 05 06 07 ;
+for EXPID in 08 09 10 11 12 13 14 15 ;
 do
     cd ${HOME}/NIPS/experiments-svhn-4/experiment_${EXPID}
     python plot.py
